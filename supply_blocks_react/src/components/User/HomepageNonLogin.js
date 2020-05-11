64d40e2a1080069f0ccc3882a2bd46cf9e4c1cce
Vaibhav Mittal <vaibhavmittal0506@gmail.com>
2020-05-11 19:27:00 +0530

    home page update

--- supply_blocks_react/src/components/User/HomepageNonLogin.js
+++ supply_blocks_react/src/components/User/HomepageNonLogin.js
@@ -1,13 +1,35 @@
-import React from 'react';
-
+import React from "react";
+import { Layout, Breadcrumb, Statistic, List, Typography, Divider } from "antd";
+const { Title, Text } = Typography;
+const data = [
+  "Login with metamask.",
+  "Add your inventory details and info.",
+  "Enjoy !!",
+];
 class HomePageNonLogin extends React.Component {
-    render(){
-        return(
-            <div>
-                    <h3>Welcome User to DashBoard!</h3>
-            </div>
-        );
-    }
+  render() {
+      var userCount=0;
+      var i=0;
+    return (
+      <div>
+        <Title level={2}>Welcome to SCAB!</Title>
+        <Title level={3}>The future of Supply Chain Management Systems</Title>
+        <List
+         size="small"
+          header={<div>Join in 3 easy Steps:</div>}
+    footer={<div><Statistic>{userCount}</Statistic> have already joined... What are you waiting for ? </div>}
+          bordered
+          dataSource={data}
+          renderItem={(item) => (
+            <List.Item>
+              <Typography.Text>{++i}. </Typography.Text> {item}
+            </List.Item>
+          )}
+        />
+        <br />
+      </div>
+    );
+  }
 }
 
 export default HomePageNonLogin;
